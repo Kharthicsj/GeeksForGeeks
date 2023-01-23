cf16465409a5ca6613fc3d20aa17db3c4490cb70
Kharthicsj <74088788+Kharthicsj@users.noreply.github.com>
2023-01-23 18:04:14 +0530

    Add files via upload

---
+++ 23-01-2023 Geeks And The String.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+    string removePair(string s) {
+        stack<char> st; 
+    
+        st.push(s[0]); 
+    
+        int curr_index = 1;
+        while (curr_index < s.length()){
+            
+            if (st.size() == 0)
+                st.push(s[curr_index]);
+            
+            else if (s[curr_index] == st.top()) 
+                st.pop();
+            else
+                st.push(s[curr_index]); 
+            curr_index += 1;
+        }
+    
+        string ans = "" ;
+        
+        while(!st.empty()) {
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        if (ans.length() ==0)
+            ans="-1";
+        
+        return ans;
+    }
+};
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		string s;
+		cin>>s;
+		solution obj;
+		string res = obj.removePair(s);
+	cout<<res<<endl;
+	}
+}
